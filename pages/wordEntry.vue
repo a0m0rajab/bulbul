6841fc0c0bc32f309c24ce9fff97ed644c7ad2fa
=Abdurrahman Rajab <=rajab-2@windowslive.com>
2020-12-30 13:14:36 +0300

    Desgin(word entry):Creating word entry form

---
+++ pages/wordEntry.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-row justify="center">
+    <v-card elevation="2" outlined shaped class="ma-3 rounded-xl">
+      <v-form v-model="valid" class="ma-3">
+        <v-jsf v-model="model" :schema="schema" />
+      </v-form>
+    </v-card>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'wordEntry',
+  data: () => ({
+    valid: false,
+    model: {},
+    schema: {
+      type: 'object',
+      properties: {
+        lang: {
+          type: 'string',
+          title: 'Choose language',
+          enum: ['Arabic', 'English', 'Turkish'],
+          'x-display': 'autocomplete',
+        },
+        word: { type: 'string' },
+        picture: { type: 'string', 'x-display': 'file' },
+      },
+    },
+  }),
+}
+</script>

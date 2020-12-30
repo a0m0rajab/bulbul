d7ef57e1f5294277cff8d9f137f4c286107f4098
=Abdurrahman Rajab <=rajab-2@windowslive.com>
2020-12-30 13:15:15 +0300

    feat(questions): Adding functions Adding functionality of multiple questions and show the correct answer.

--- components/multipleChoise.vue
+++ components/multipleChoise.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container>
     <v-row>
-      <v-card elevation="2" outlined shaped class="rounded-xl">
+      <v-card elevation="2" outlined shaped class="rounded-xl ma-2">
         <v-card-text class="d-flex justify-center text-center">
           {{ questionText }}
         </v-card-text>
@@ -13,21 +13,29 @@
           <v-card
             @click="buttonClicked(item.answer)"
             outlined
-            :color="(item.answer&&selected)?`green`:null"
+            :color="item.answer && selected ? `green` : null"
             shaped
             :elevation="hover ? 16 : 2"
             :class="{ 'on-hover': hover }"
             style="cursor: pointer"
             class="rounded-xl d-flex justify-center"
           >
-            <v-card-text class="d-flex justify-center"> {{ item.text }}</v-card-text>
+            <v-card-text class="d-flex justify-center">
+              {{ item.text }}</v-card-text
+            >
             <img src="~/assets/animals/panda.png" />
           </v-card>
         </v-hover>
       </v-col>
     </v-row>
     <v-row>
-      <v-card @click="nextQuestion" v-if="selected" outlined shaped class="rounded-xl">
+      <v-card
+        @click="nextQuestion"
+        v-if="selected"
+        outlined
+        shaped
+        class="rounded-xl"
+      >
         <v-card-text class="d-flex justify-center text-center">
           The button is clicked {{ correct }}
         </v-card-text>
@@ -39,28 +47,26 @@
 <script>
 export default {
   data: function () {
-    return { selected: 0,correct:false };
+    return { selected: 0, correct: false }
   },
   methods: {
-    nextQuestion(){
-        if(this.$store.state.question.questionNo < 3){
-
+    nextQuestion() {
+      if (this.$store.state.question.questionNo < 3) {
         this.$store.commit('question/nextQuestion')
-        this.correct= false;
-        this.selected = false;
-        }
+        this.correct = false
+        this.selected = false
+      }
     },
     buttonClicked(event) {
-    if (!this.selected) {
-        this.correct=!!event;
-        this.selected = true;
-    }
-    
+      if (!this.selected) {
+        this.correct = !!event
+        this.selected = true
+      }
     },
   },
   props: {
     questionText: String,
     options: Array,
   },
-};
+}
 </script>
